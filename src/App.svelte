3089937eeb5be19d1068e652fbab51417c867ca2
Yerko Palma <yerko.palma.serrano@outlook.com>
2025-07-17 21:57:12 -0400

    added Taskbar component

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,11 @@
 <script>
   import Window from './lib/Window.svelte';
+  import Taskbar from './lib/Taskbar.svelte';
 </script>
 
-  <Window title = {"My Custom Window"}>
-    <p>This is the content of the window.</p>
-    <p>You can add more elements here as needed.</p>
-  </Window>
+<Window title = {"My Custom Window"}>
+  <p>This is the content of the window.</p>
+  <p>You can add more elements here as needed.</p>
+</Window>
+<Taskbar>
+</Taskbar>
